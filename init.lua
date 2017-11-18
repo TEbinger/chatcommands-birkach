557dc517744435ef023907e770962a247c043b1d
TEbinger <thomas.ebinger@gmx.de>
2017-11-18 21:44:43 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+ minetest.register_chatcommand("ichhabeverstanden", {description = "Gibt dir Rechte zum Bauen",
+ func = function(name, param)    
+ local privs = minetest.get_player_privs(name)    
+ privs.interact = true    
+ privs.zoom = true    
+ privs.shout = true    
+ privs.home = true    
+ minetest.set_player_privs(name, privs)  end}) 
