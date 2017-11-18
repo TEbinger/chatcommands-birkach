c284fcad566776b897343145c9df815e69f4e635
TEbinger <thomas.ebinger@gmx.de>
2017-11-18 23:01:26 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,17 +1,13 @@
 minetest.register_chatcommand("ichhabeverstanden", {
-  params = "",
-  description = "Gibt dir Rechte zum Bauen",
-    func = function(name, param)
-      local privs = minetest.get_player_privs(name)     
-      privs.interact = true    
-      privs.zoom = true    
-      privs.shout = true    
-      privs.home = true   
-      minetest.set_player_privs(name, privs)
-    return true, "Du darfst jetzt Bauen."
+ params = "",
+ description = "Gibt dir Rechte zum Bauen",
+  func = function(name, param)
+   local privs = minetest.get_player_privs(name)
+   privs.interact = true
+   privs.zoom = true
+   privs.shout = true
+   privs.home = true
+   minetest.set_player_privs(name, privs)
+   return true, "Du darfst jetzt Bauen."
   end,
 })
-
---Siehe http://dev.minetest.net/chatcommand  
- 
- 
