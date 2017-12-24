1d970b19006a444de52b3f6ca032aa81f5516c91
TEbinger <thomas.ebinger@gmx.de>
2017-12-24 11:23:00 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,13 +1,29 @@
-minetest.register_chatcommand("ichhabeverstanden", {
+minetest.register_chatcommand("dankefuerdasgeschenk", {
  params = "",
- description = "Gibt dir Rechte zum Bauen",
+ description = "Gibt dir Flugrechte",
   func = function(name, param)
    local privs = minetest.get_player_privs(name)
+   privs.fly = true
    privs.interact = true
    privs.zoom = true
    privs.shout = true
    privs.home = true
    minetest.set_player_privs(name, privs)
-   return true, "Du darfst jetzt Bauen."
+   return true, "Du kannst jetzt fliegen. Aktiviere/deaktiviere es mit der Taste K"
   end,
 })
+
+
+--minetest.register_chatcommand("ichhabeverstanden", {
+-- params = "",
+-- description = "Gibt dir Rechte zum Bauen",
+--  func = function(name, param)
+--   local privs = minetest.get_player_privs(name)
+--   privs.interact = true
+--   privs.zoom = true
+--   privs.shout = true
+--   privs.home = true
+--   minetest.set_player_privs(name, privs)
+--   return true, "Du darfst jetzt Bauen."
+--  end,
+--})
